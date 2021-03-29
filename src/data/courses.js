14c196c68093ead2aa03991c052339eb25c2e88f
criselle albor <alborcriselle10@gmail.com>
2021-03-29 11:03:03 +0800

    change admin dashboard on courses.js page

--- src/data/courses.js
+++ src/data/courses.js
@@ -4,21 +4,27 @@ let coursesData = [
         name: "PHP-Laravel",
         description: "lorem ipsum samina mina e e",
         price: 40000,
-        onOffer: true
+        onOffer: true,
+        start_date: '2021-02-20',
+        end_date: '2021-08-19'
     },
     {
         id: "wdc002",
         name: "Phython-Django",
         description: "waka waka e e",
         price: 45000,
-        onOffer: true
+        onOffer: true,
+        start_date: '2021-02-20',
+        end_date: '2021-08-19'
     },
     {
         id: "wdc003",
         name: "Java-Springboot",
         description: "Today's yor day, I feel it. You paved the way, believe it.",
         price: 55000,
-        onOffer: true
+        onOffer: true,
+        start_date: '2021-02-20',
+        end_date: '2021-08-19'
     },  
 
 ];
--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Banner from '../components/Banner';
 import Course from '../components/Course';
 import coursesData from '../data/courses';
+import UserContext from '../UserContext';
+import {Table, Button} from 'react-bootstrap';
 
 export default function Courses(){
-
+	// use the userContext and destructure it to access the user state defined in the App component
+	const {user} = useContext(UserContext);
+	
 	/*
 		JSX does not alloq components to return adjacent JSX 
 		elements. It should be wrapped in a single element.
@@ -30,7 +34,45 @@ export default function Courses(){
 		)
 	})
 	console.log(courses)
+
+	const coursesRows = coursesData.map( course => {
+		return(
+			<tr key={course.id}>
+				<td>{course.id}</td>
+				<td>{course.name}</td>
+				<td>PhP {course.price}</td>
+				<td>{course.onOffer ? 'open' : 'closed'}</td>
+				<td>{course.start_date}</td>
+				<td>{course.end_date}</td>
+				<td>
+					<Button variant="warning">Update</Button>
+					<Button variant="danger">Disable</Button>
+				</td>
+			</tr>
+		)
+	})
+
 	return(
+		user.isAdmin === true ? 
+		<>
+			<h1>code for admin</h1>
+			<Table>
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>name</th>
+						<th>Price</th>
+						<th>Status</th>
+						<th>Start Date</th>
+						<th>End Date</th>
+						<th>Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+					{coursesRows}
+				</tbody>
+			</Table>
+		</> :
 		<React.Fragment>
 			<Banner dataProp={data}/>
 			{courses}
